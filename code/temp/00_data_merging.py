fddd46e07085a94a4e667a1c7a82be974324e09c
Marcin Rydelski <Marcin.Rydelski@tomtom.com>
2019-06-06 09:51:05 +0200

    Initial commit

---
+++ code/temp/00_data_merging.py
@@ -0,0 +1,8 @@
+import os
+import glob
+import pandas as pd
+
+path = "d:/edu/pandas_dask_spark/data/raw"
+files = glob.glob(os.path.join(path, "pojazdy*.csv"))
+df = pd.concat(pd.read_csv(file, low_memory=False) for file in files)
+df.to_csv("d:/edu/pandas_dask_spark/data/raw/Vehicles_merged.csv")
