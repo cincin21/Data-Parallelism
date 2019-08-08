bf370b00c42895486a51887c1f9356182d7cd341
Marcin Rydelski <Marcin.Rydelski@tomtom.com>
2019-08-08 19:08:24 +0200

    Updated Spark analysis and data vehicles_data_merging script

--- code/vehicles_data_merging.py
+++ code/vehicles_data_merging.py
@@ -2,7 +2,7 @@ import os
 import glob
 import pandas as pd
 
-path = "d:/edu/pandas_dask_spark/data/raw"
+path = "../data/raw"
 files = glob.glob(os.path.join(path, "pojazdy*.csv"))
 df = pd.concat(pd.read_csv(file, low_memory=False) for file in files)
-df.to_csv("../../data/raw/Vehicles_merged.csv")
+df.to_csv("../data/raw/Vehicles.csv")
